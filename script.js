0fdf497027d833c3e1d17ef9316226d8900c21fb
yu-iyo <garypoopoo51@gmail.com>
2023-10-20 16:01:11 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
